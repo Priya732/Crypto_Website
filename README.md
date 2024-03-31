e73321e99e91e0ea1bcb3a07d97ef66b6d9e21b5
Priyabrata <sahoopriya732@gmail.com>
2024-03-31 08:39:56 +0530

    Project Completed

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is a crypto Website made using HTML,CSS,JS
+
+An API is Fetched for updation of Real time value of the coins
---
+++ script.js
@@ -0,0 +1,16 @@
+const btc=document.getElementById('bitcoin')
+const eth=document.getElementById('ethereum')
+const dog=document.getElementById('dogecoin')
+
+const settings={
+    'async':true,
+    'scrossDomain':true,
+    'url':'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=inr',
+    'method':'GET',
+    'headers':{}
+}
+$.ajax(settings).done(function(response){
+    btc.innerHTML=response.bitcoin.inr
+    eth.innerHTML=response.ethereum.inr
+    dog.innerHTML=response.dogecoin.inr
+})
